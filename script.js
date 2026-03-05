25082fd9fd647111e21586f41bc5e630739c885a
0815311016-beep <0815311016@s.tusd1.org>
2026-03-05 15:42:09 +0000

    i tried

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-consl
+console.log("Script started");
+
+
+//variable to track the number of clicks
+let clicks = 0;
+//value of each click
+let clickValue = 1;
+
+//variable to store the click counter h1
+let clickDisplay = document.getElementById(click-display);
+// Store level display
+let levelDisplay = document.getElementById("level-display");
+
+//set initial text for click counter
+clickDisplay.innerText = "clicks:" + clicks;
+
+//called each time the image is clicked
+function handleClick() {
+    console.log("Click")
+    //Add one to click count
+    clicks = clicks + clickValue;
+    //click count display for h1
+    clickDisplay.innerText = "Clicks" + clicks;
+}
